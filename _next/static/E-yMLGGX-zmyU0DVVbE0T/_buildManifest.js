8646be9432fea12672aeb1b8376839c522630f04
jstryja <53625111+jstryja@users.noreply.github.com>
2023-02-21 20:13:16 +0000

    Deploying to gh-pages from @ jstryja/form-list@6362cf96bd1e59769b26f90096398c141ada882a 🚀

---
+++ _next/static/E-yMLGGX-zmyU0DVVbE0T/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/bcdce425-754f2d35d91a827b.js","static/chunks/180-889e478ad3296f9e.js","static/chunks/pages/index-04751e6d73329f63.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
