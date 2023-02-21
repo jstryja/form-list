65b67ef8afbea000f1b69d6c4d8c345df328603a
Kornelius Rakoczy <kornelius.rakoczy@gmail.com>
2023-02-21 20:54:09 +0100

    fix: asset prefix for build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import {Button, Col, Divider, message, Row, Space} from "antd";
-import {useEffect, useState} from "react";
+import { Button, Col, Divider, message, Row, Space, Upload } from 'antd';
 import csvToJson from 'csvtojson';
-import {Upload} from 'antd';
+import { useEffect, useState } from 'react';
 
-const {Dragger} = Upload;
+const { Dragger } = Upload;
 
 interface RowInterface {
     profession: string;
@@ -24,23 +23,25 @@ interface RowInterface {
 export default function Home() {
     const [file, setFile] = useState<string>();
     const [data, setData] = useState<RowInterface[]>([]);
-    const [professionDetails, setProfessionDetails] = useState<RowInterface | undefined>()
+    const [professionDetails, setProfessionDetails] = useState<RowInterface | undefined>();
     const update = () => {
         const ls = localStorage.getItem('firm-list');
-        csvToJson().fromString(ls as string).then((json) => {
-            setData(json);
-            setProfessionDetails(json[0])
-        });
-    }
+        if (!ls) return;
+        csvToJson()
+            .fromString(ls as string)
+            .then((json) => {
+                setData(json);
+                setProfessionDetails(json[0]);
+            });
+    };
     useEffect(() => {
         if (typeof window !== 'undefined') {
             update();
         }
-    }, [])
-
+    }, []);
 
     return (
-        <div style={{height: '100vh', background: 'white', padding: '1rem'}}>
+        <div style={{ height: '100vh', background: 'white', padding: '1rem' }}>
             <Row>
                 <Col>
                     <Space>
@@ -58,64 +59,69 @@ export default function Home() {
                                 if (info.file.status === 'done') {
                                     const reader = new FileReader();
                                     reader.onload = (e) => {
-                                        if (typeof e.target?.result === 'string')
-                                            setFile(e.target.result);
-                                    }
+                                        if (typeof e.target?.result === 'string') setFile(e.target.result);
+                                    };
                                     if (info.file.originFileObj) {
                                         reader.readAsText(info.file.originFileObj);
                                     }
-
                                 }
-                            }
-                            }
+                            }}
                         >
                             <p className="ant-upload-text">Nahrej CSV</p>
                         </Dragger>
-                        <Button type={'primary'} onClick={() => {
-                            localStorage.setItem('firm-list', file || '')
-                            message.success('Soubor nahrán.');
-                            update();
-                        }
-                        }>ODESLAT</Button>
+                        <Button
+                            type={'primary'}
+                            onClick={() => {
+                                localStorage.setItem('firm-list', file || '');
+                                message.success('Soubor nahrán.');
+                                update();
+                            }}
+                        >
+                            ODESLAT
+                        </Button>
                     </Space>
                 </Col>
-                <Divider/>
+                <Divider />
             </Row>
 
             <Row>
                 <Col span={4}>
                     <Space direction="vertical">
                         {data.map((row, index) => (
-                            <Button key={index}
-                                    type={row.profession === professionDetails?.profession ? 'primary' : 'default'}
-                                    style={{display: 'block'}} onClick={() => {
-                                setProfessionDetails(row)
-                            }}>
+                            <Button
+                                key={index}
+                                type={row.profession === professionDetails?.profession ? 'primary' : 'default'}
+                                style={{ display: 'block' }}
+                                onClick={() => {
+                                    setProfessionDetails(row);
+                                }}
+                            >
                                 {index + 1}. {row.profession}
                             </Button>
                         ))}
                     </Space>
                 </Col>
                 <Col span={10}>
-                    <Divider type={'vertical'} style={{height: '100%'}}/>
-
-                    {professionDetails?.col1row1} <br/>
-                    {professionDetails?.col1row2} <br/>
-                    {professionDetails?.col1row3} <br/><br/>
-                    {professionDetails?.col1row4} <br/>
-                    {professionDetails?.col1row5} <br/>
-                    {professionDetails?.col1row6} <br/>
+                    <Divider type={'vertical'} style={{ height: '100%' }} />
+                    {professionDetails?.col1row1} <br />
+                    {professionDetails?.col1row2} <br />
+                    {professionDetails?.col1row3} <br />
+                    <br />
+                    {professionDetails?.col1row4} <br />
+                    {professionDetails?.col1row5} <br />
+                    {professionDetails?.col1row6} <br />
                 </Col>
                 <Col span={10}>
-                    <Divider type={'vertical'} style={{height: '100%'}}/>
-                    {professionDetails?.col2row1} <br/>
-                    {professionDetails?.col2row2} <br/>
-                    {professionDetails?.col2row3} <br/><br/>
-                    {professionDetails?.col2row4} <br/>
-                    {professionDetails?.col2row5} <br/>
-                    {professionDetails?.col2row6} <br/>
+                    <Divider type={'vertical'} style={{ height: '100%' }} />
+                    {professionDetails?.col2row1} <br />
+                    {professionDetails?.col2row2} <br />
+                    {professionDetails?.col2row3} <br />
+                    <br />
+                    {professionDetails?.col2row4} <br />
+                    {professionDetails?.col2row5} <br />
+                    {professionDetails?.col2row6} <br />
                 </Col>
             </Row>
         </div>
-    )
+    );
 }
