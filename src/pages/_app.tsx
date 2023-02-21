d2307a39284ed3f80312b7e93ad7c09868484df0
Jakub Stryja <jakub.stryja@atreo.cz>
2023-02-21 17:41:44 +0100

    antd css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 // import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
+// import './index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
