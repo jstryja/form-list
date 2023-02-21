fa5090737f53b41ebb9b4b520195851f0363232a
jstryja <53625111+jstryja@users.noreply.github.com>
2023-02-21 15:39:01 +0000

    Deploying to gb-pages from @ jstryja/form-list@6480433759d779de44aa7d54f6b706cbd06f2bea 🚀

---
+++ _next/static/WQ_-vWoKZnn91lVbA0DLd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/210-e6e4c80c8c7ccbc3.js","static/chunks/pages/index-bf5528e438097b47.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
