a22e14ffc71ccb8c1041811f9badab53a629405c
KorneliusRakoczy <53625121+KorneliusRakoczy@users.noreply.github.com>
2023-02-21 19:55:09 +0000

    Deploying to gh-pages from @ jstryja/form-list@65b67ef8afbea000f1b69d6c4d8c345df328603a 🚀

--- _next/static/grrBPoSIDVVR2xbrUznX7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/bcdce425-754f2d35d91a827b.js","static/chunks/180-889e478ad3296f9e.js","static/chunks/pages/index-37c3d9d5504f236e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
